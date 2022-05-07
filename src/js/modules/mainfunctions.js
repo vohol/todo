f1af4a69247d12c6483fd6a00cabc6e42002cf94
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-05-07 09:34:07 +0300

    added styles

--- src/js/modules/mainfunctions.js
+++ src/js/modules/mainfunctions.js
@@ -1,39 +1,50 @@
 export function newNightTheme(classOfSection) {
-  const section = document.querySelector(`.${classOfSection}`)
+  const section = document.querySelector(`.${classOfSection}`);
   let newTheme;
 
   switch (section.dataset.theme) {
-    case 'night':
-      newTheme = 'day'
+    case "night":
+      newTheme = "day";
       break;
-    case 'day':
-      newTheme = 'night'
+    case "day":
+      newTheme = "night";
       break;
   }
-  section.dataset.theme = newTheme
+  section.dataset.theme = newTheme;
 }
 
 export function setTheme(classOfSection) {
-  const section = document.querySelector(`.${classOfSection}`)
-  let fontColor; let taskBg;
+  const section = document.querySelector(`.${classOfSection}`);
+  let fontColor;
+  let taskBg;
   let borderColor;
+  let placeHolderColor;
+  let btnTextColor;
+  let shadow;
 
   switch (section.dataset.theme) {
-    case 'night':
-      fontColor = '#fff'
-      taskBg = '#25273D'
-      borderColor = '#393A4B'
-      placeHolderColor = '#767992'
+    case "night":
+      fontColor = "#fff";
+      taskBg = "#25273D";
+      borderColor = "#393A4B";
+      placeHolderColor = "#767992";
+      btnTextColor = "#5B5E7E";
+      shadow = '0px 35px 50px -15px rgba(0, 0, 0, 0.5)'
       break;
-    case 'day':
-      fontColor = '#000'
-      taskBg = '#fff'
-      borderColor = '#E3E4F1'
-      placeHolderColor = '#9495A5'
+    case "day":
+      fontColor = "#000";
+      taskBg = "#fff";
+      borderColor = "#E3E4F1";
+      placeHolderColor = "#9495A5";
+      btnTextColor = "#9495A5";
+      shadow = '0px 35px 50px -15px rgba(194, 195, 214, 0.5)'
       break;
   }
   section.style.setProperty("--font-color", fontColor);
   section.style.setProperty("--task-bg", taskBg);
   section.style.setProperty("--border-color", borderColor);
   section.style.setProperty("--place-holder-color", placeHolderColor);
+  section.style.setProperty("--btn-text-color", btnTextColor);
+  section.style.setProperty("--shadow", shadow);
 }
+
