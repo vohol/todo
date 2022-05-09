4cf6e4191ca5aee8db99de507e20c41bfa85fb65
Vladimir Kholiavskiy <kholiavskiy@gmail.com>
2022-05-09 10:12:48 +0300

    added parsing function

--- src/js/app.js
+++ src/js/app.js
@@ -6,10 +6,18 @@ dfltFunctions.isWebp();
 document.addEventListener('DOMContentLoaded', function () {
   //load default values
   mf.setTheme('todo')
+  mf.parseTasksAndAddToTaskManager()
 })
 
+
+//toggle themes
 const themeToggle = document.querySelector('.todo__theme-toggle')
 themeToggle.addEventListener('click', function () {
   mf.newNightTheme('todo')
   mf.setTheme('todo')
+})
+
+const btnToAddNewTask = document.querySelector('.new-task__btn')
+btnToAddNewTask.addEventListener('click', function () {
+  mf.addTask('new-task', 'new-task-done');
 })
--- src/js/modules/mainfunctions.js
+++ src/js/modules/mainfunctions.js
@@ -1,3 +1,4 @@
+//function for toggle themes for section
 export function newNightTheme(classOfSection) {
   const section = document.querySelector(`.${classOfSection}`);
   let newTheme;
@@ -13,6 +14,7 @@ export function newNightTheme(classOfSection) {
   section.dataset.theme = newTheme;
 }
 
+//function to set theme for section
 export function setTheme(classOfSection) {
   const section = document.querySelector(`.${classOfSection}`);
   let fontColor;
@@ -48,3 +50,107 @@ export function setTheme(classOfSection) {
   section.style.setProperty("--shadow", shadow);
 }
 
+export function addTask(inputID, idMarker) {
+  const input = document.getElementById(inputID)
+  const doneMarker = document.getElementById(idMarker)
+  let task = {
+    id: Date.now(),
+    task: input.value,
+    status: doneMarker.checked ? Date.now() : 'active',
+  }
+  console.log(task);
+  addTaskToStorage(task)
+  input.value = ''
+  parseTasksAndAddToTaskManager()
+}
+
+export function addTaskToStorage(newTask) {
+  let getAllTasks = JSON.parse(localStorage.getItem('storage'));
+  getAllTasks ? getAllTasks : getAllTasks = []
+  getAllTasks.push(newTask);
+  JSON.stringify(getAllTasks)
+  localStorage.setItem('storage', JSON.stringify(getAllTasks))
+}
+
+export function parseTasksAndAddToTaskManager() {
+  let getAllTasks = JSON.parse(localStorage.getItem('storage'));
+
+  const taskManager = document.querySelector('.task-manager')
+  taskManager.innerHTML = ''
+
+
+  const activeTasks = getAllTasks.filter(task => task.status == 'active')
+  const doneTasks = getAllTasks.filter(task => task.status != 'active')
+
+  const sortedActiveTasks = activeTasks.sort(function (a, b) {
+    return b.id - a.id;
+  })
+  const SortedDoneTasks = doneTasks.sort(function (a, b) {
+    return b.status - a.status;
+  })
+
+  sortedActiveTasks.forEach(element => {
+
+    let task = document.createElement("li");
+    task.classList.add('task-manager__item-list')
+    task.innerHTML = `
+  <label class="task-block task-manager__task">
+    <label class="main-checkbox task-block__checkbox">
+      <input type="checkbox" class="main-checkbox__check">
+      <span class="main-checkbox__mark"></span>
+    </label>
+    <span class="task-block__text">${element.task}</span>
+    <button class="task-block__btn">
+      <svg class="close-icon task-block__icon">
+        <use xlink:href="@img/icons/icons-mono.svg#close"></use>
+      </svg>
+    </button>
+  </label>
+  `;
+    taskManager.appendChild(task)
+  });
+
+  SortedDoneTasks.forEach(element => {
+
+    let task = document.createElement("li");
+    task.classList.add('task-manager__item-list')
+    task.classList.add('done')
+    task.innerHTML = `
+  <label class="task-block task-manager__task">
+    <label class="main-checkbox task-block__checkbox">
+      <input type="checkbox" class="main-checkbox__check" checked>
+      <span class="main-checkbox__mark"></span>
+    </label>
+    <span class="task-block__text">${element.task}</span>
+    <button class="task-block__btn">
+      <svg class="close-icon task-block__icon">
+        <use xlink:href="@img/icons/icons-mono.svg#close"></use>
+      </svg>
+    </button>
+  </label>
+  `;
+    taskManager.appendChild(task)
+  });
+
+
+  //addding task
+  /*
+  const task = document.createElement("li");
+  task.classList.add('task-manager__item-list')
+  task.innerHTML = `
+  <label class="task-block task-manager__task">
+    <label class="main-checkbox task-block__checkbox">
+      <input type="checkbox" class="main-checkbox__check">
+      <span class="main-checkbox__mark"></span>
+    </label>
+    <span class="task-block__text">${task}</span>
+    <button class="task-block__btn">
+      <svg class="close-icon task-block__icon">
+        <use xlink:href="@img/icons/icons-mono.svg#close"></use>
+      </svg>
+    </button>
+  </label>
+  `;
+*/
+
+}
